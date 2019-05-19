4ef6a7c0671f3a6e4e74b161ceb099a4e38e9b02
tomas <tomas.arrufat@gmail.com>
2019-05-19 12:10:12 +0200

    Finished PrototypeViaSerialization

--- design-patterns-modern-cpp/Prototypes/PrototypeViaSerialization/src/PrototypeViaSerialization.cpp
+++ design-patterns-modern-cpp/Prototypes/PrototypeViaSerialization/src/PrototypeViaSerialization.cpp
@@ -2,41 +2,50 @@
 #include <memory>
 using namespace std;
 
-#include <boost/serializarion/serialization.hpp>
+#include <boost/serialization/serialization.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
-using namespace boost;
-
 
 struct Address
 {
     string street, city;
     int room;
 
-    Address(const string &street, const string &city, const int room):
-        street(street), city(city), room(room) {};
+    Address() {};
 
-    Address(const Address &other) :
-        street(other.street), city(other.city), room(other.room) {};
+    Address(const string &street, const string &city, const int room) : street(street), city(city), room(room){};
+
+    Address(const Address &other) : street(other.street), city(other.city), room(other.room){};
 
     friend ostream &operator<<(ostream &os, const Address &address)
     {
-        os << "street: " << address.street << " city: " << address.city <<
-        " room: " << address.room;
+        os << "street: " << address.street << " city: " << address.city << " room: " << address.room;
+    }
+
+private:
+    friend class boost::serialization::access;
+
+    template <class archive>
+    void serialize(archive & ar, const unsigned int version)
+    {
+        ar & street;
+        ar & city;
+        ar & room;
     }
 };
 
 struct Contact
 {
     string name;
-    Address* address;
+    Address *address;
 
-    Contact(const string &name, Address* address):
-        name(name), address(address) {};
+    Contact(){};
+
+    Contact(const string &name, Address *address) : name(name), address(address){};
 
     Contact(const Contact &other) : //Copy constructor
-        name(other.name),
-        address( new Address{ *other.address}) {};
+                                    name(other.name),
+                                    address(new Address{*other.address}){};
 
     friend ostream &operator<<(ostream &os, const Contact &contact)
     {
@@ -44,30 +53,58 @@ struct Contact
         return os;
     }
 
+private:
+    friend class boost::serialization::access;
+
+    template <class archive>
+    void serialize(archive & ar, const unsigned int version)
+    {
+        ar & name;
+        ar & address;
+    }
 };
 
 struct EmployeeFactory
 {
     static unique_ptr<Contact> new_main_office_employee(const string &name, int room)
     {
-        static Contact mainOfficeEmployee{"",new Address{"123 East Dr", "London", 0}};
+        static Contact mainOfficeEmployee{"", new Address{"123 East Dr", "London", 0}};
         return new_employee(name, room, mainOfficeEmployee);
     }
-    private:
+
+private:
     static unique_ptr<Contact> new_employee(const string &name, int room, const Contact &prototype)
     {
-         auto result = make_unique<Contact>(prototype);
-         result->name = name;
-         result->address->room = room;
-         return result;
+        auto result = make_unique<Contact>(prototype);
+        result->name = name;
+        result->address->room = room;
+        return result;
     }
 };
 
 int main()
 {
-    auto john = EmployeeFactory::new_main_office_employee("Jhon Doe", 125);
-    auto jane = EmployeeFactory::new_main_office_employee("Jane Smith", 126);
+    auto clone = [](const Contact& c)
+    {
+        ostringstream oss;
+        boost::archive::text_oarchive oa(oss);
+        oa << c;
+        string s = oss.str();
+        cout << s << endl;
+        istringstream iss(s);
+        boost::archive::text_iarchive ia(iss);
+        Contact result;
+        ia >> result;
+        return result;
+    };
 
-    cout << *john << endl << *jane << endl;
+    auto john = EmployeeFactory::new_main_office_employee("Jhon Doe", 125);
+
+    auto jane = clone(*john);
+    jane.address->room = 126;
+    jane.name = "Jane Smith";
+
+    cout << *john << endl
+         << jane << endl;
     return 0;
 }
